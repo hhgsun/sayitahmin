0bcfe52c408f36013b0dff78a4b59a70d901c9b5
Mac9 <mac9@Mac9s-iMac.local>
2021-06-12 15:11:54 +0300

    first

---
+++ sayitahmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sayitahmin
+//
+//  Created by Mac9 on 12.06.2021.
+//  Copyright © 2021 hhgsun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
