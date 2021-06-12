5537df0cdab2edcfd9c93857ba1847b0b8c3ef20
Mac9 <mac9@Mac9s-iMac.local>
2021-06-12 16:07:53 +0300

    core build

--- sayitahmin/ViewController.swift
+++ sayitahmin/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue: Int = 0
+    var targetValue: Int = 0;
+    
+    @IBOutlet weak var lblTarget: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        newRound()
     }
 
-
+    @IBAction
+    func buttonClicked(){
+        if( currentValue == targetValue ) {
+            showMessage(title: "Aferim", message: "Tahminin iyi \(currentValue)")
+        } else {
+            showMessage(title: "Kötü Haber", message: "Tahminin Tutmadı \nHedef: \(targetValue) \nSeçimin: \(currentValue)")
+        }
+        newRound()
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider){
+        currentValue = lroundf(slider.value)
+    }
+    
+    func showMessage(title:String, message: String){
+        let alert = UIAlertController(title: title, message:message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok Cnm", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func newRound(){
+        targetValue = 1 + Int(arc4random_uniform(100))
+        lblTarget.text = "\(targetValue)"
+        currentValue = 0
+    }
 }
 
